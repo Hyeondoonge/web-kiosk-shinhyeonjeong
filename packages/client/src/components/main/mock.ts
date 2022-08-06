818a5b72e89335c399284120dcf650271df45fbe
Hyeondoonge <migu554@naver.com>
2022-08-06 16:34:11 +0900

    chore: 목데이터 생성

---
+++ packages/client/src/components/main/mock.ts
@@ -0,0 +1,128 @@
+export const mock = [
+  {
+    id: 1,
+    name: '커피',
+    menuList: [
+      {
+        id: 1,
+        name: '아메리카노',
+        imgUrl: '',
+        price: 1000,
+        sellCount: 5,
+        categoryId: 1,
+      },
+      {
+        id: 4,
+        name: '헤이즐넛 커피',
+        imgUrl: '',
+        price: '2100',
+        sellCount: 15,
+        categoryId: 1,
+      },
+      {
+        id: 5,
+        name: '콜드브루',
+        imgUrl: '',
+        price: 1500,
+        sellCount: 15,
+        categoryId: 1,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: '라떼',
+    menuList: [
+      {
+        id: 2,
+        name: '돌체라떼',
+        imgUrl: '',
+        price: 1000,
+        sellCount: 5,
+        categoryId: 1,
+      },
+      {
+        id: 3,
+        name: '카페라떼',
+        imgUrl: '',
+        price: 3500,
+        sellCount: 15,
+        categoryId: 1,
+      },
+      {
+        id: 6,
+        name: '티라미수 라떼',
+        imgUrl:
+          'http://www.mmthcoffee.com/data/file/mm_coffee/thumb-3546841901_uvxqasKz_188f95a31eb70a833360b579a3508c3dc04b2f42_216x216.png',
+        price: 8000,
+        sellCount: 15,
+        categoryId: 1,
+      },
+      {
+        id: 7,
+        name: '꿀바나 라떼',
+        imgUrl:
+          'http://www.mmthcoffee.com/data/file/mm_coffee/thumb-3553807308_JwPNkzO7_350ae9f1435c2a576b345535e575353bb9e05bc8_216x216.jpg',
+        price: 5000,
+        sellCount: 15,
+        categoryId: 1,
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: '쥬스',
+    menuList: [
+      {
+        id: 2,
+        name: '오렌지 쥬스',
+        imgUrl: '',
+        price: 1000,
+        sellCount: 5,
+        categoryId: 1,
+      },
+      {
+        id: 3,
+        name: '포도 쥬스',
+        imgUrl: '',
+        price: 3500,
+        sellCount: 15,
+        categoryId: 1,
+      },
+      {
+        id: 6,
+        name: '청포도 쥬스',
+        imgUrl:
+          'http://www.mmthcoffee.com/data/file/mm_coffee/thumb-3546841901_uvxqasKz_188f95a31eb70a833360b579a3508c3dc04b2f42_216x216.png',
+        price: 8000,
+        sellCount: 15,
+        categoryId: 1,
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: '티',
+    menuList: [
+      {
+        id: 6,
+        name: '로얄 밀크티스',
+        imgUrl:
+          'http://www.mmthcoffee.com/data/file/mm_coffee/thumb-3546841901_uvxqasKz_188f95a31eb70a833360b579a3508c3dc04b2f42_216x216.png',
+        price: 8000,
+        sellCount: 15,
+        categoryId: 1,
+      },
+
+      {
+        id: 10,
+        name: '녹차',
+        imgUrl:
+          'http://www.mmthcoffee.com/data/file/mm_coffee/thumb-3546841901_uvxqasKz_188f95a31eb70a833360b579a3508c3dc04b2f42_216x216.png',
+        price: 8000,
+        sellCount: 15,
+        categoryId: 1,
+      },
+    ],
+  },
+]

b6d1470373fdfc4d0d14281807d79ee58052c8fc
Hyeondoonge <migu554@naver.com>
2022-08-08 00:38:22 +0900

    feat: 메인화면에 필요한 상태 추가

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import CategoryList from 'components/main/CategoryList'
+import MenuList from 'components/main/MenuList'
+import MenuOptionSelector from 'components/main/MenuOptionSelector'
+import React, { useEffect, useState } from 'react'
+import { CategoryType, MenuType } from 'type'
+import mock from './mock/mockCategoryList.json'
+import mockOptionList from './mock/mockOptionList.json'
 
 function App() {
-  return <div className="App"></div>
+  const [selectedCategoryId, setSelectedCategoryId] = useState(-1)
+  const [categoryList, setCategoryList] = useState<CategoryType[]>([])
+  const [menuList, setMenuList] = useState<MenuType[]>([])
+
+  useEffect(() => {
+    setCategoryList(mock)
+  }, [])
+
+  const updateSelectedCategoryId = (newSelectedCategoryId: number) => {
+    setSelectedCategoryId(newSelectedCategoryId)
+    setMenuList(
+      categoryList.filter(({ id }) => id === newSelectedCategoryId)[0].menuList
+    )
+  }
+
+  return (
+    <div className="App">
+      <CategoryList
+        categoryList={categoryList}
+        selectedCategoryId={selectedCategoryId}
+      />
+      <MenuList menuList={menuList} />
+      <MenuOptionSelector
+        menu={mock[0].menuList[0]}
+        optionList={mockOptionList}
+      />
+    </div>
+  )
 }
 
 export default App

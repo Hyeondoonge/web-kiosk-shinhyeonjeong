6d3933763fb1237e08f5bf26e757eececfc10d6f
Hyeondoonge <migu554@naver.com>
2022-08-08 10:18:53 +0900

    feat: 헤더 메뉴 클릭에 따른 메뉴 리스트 변경

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,42 +1,46 @@
+import CachePayment from 'components/main/CachePayment'
 import Cart from 'components/main/Cart'
 import CategoryList from 'components/main/CategoryList'
 import MenuList from 'components/main/MenuList'
 import MenuOptionSelector from 'components/main/MenuOptionSelector'
+import Receipt from 'components/main/Receipt'
 import React, { useEffect, useState } from 'react'
 import { CategoryType, MenuType, SelectedMenuType } from 'type'
 import mock from './mock/mockCategoryList.json'
 import mockOptionList from './mock/mockOptionList.json'
+import order from './mock/mockOrderedMenuList.json'
 
 function App() {
   const [selectedCategoryId, setSelectedCategoryId] = useState(-1)
   const [categoryList, setCategoryList] = useState<CategoryType[]>([])
-  const [menuList, setMenuList] = useState<MenuType[]>([])
   const [selectedMenuList, setSelectedMenuList] = useState<SelectedMenuType[]>(
     []
   )
-
   useEffect(() => {
     setCategoryList(mock)
   }, [])
 
   const updateSelectedCategoryId = (newSelectedCategoryId: number) => {
     setSelectedCategoryId(newSelectedCategoryId)
-    setMenuList(
-      categoryList.filter(({ id }) => id === newSelectedCategoryId)[0].menuList
-    )
   }
+
+  const menuList =
+    categoryList.filter(({ id }) => id === selectedCategoryId)[0]?.menuList ??
+    []
 
   return (
     <div className="App">
       <CategoryList
         categoryList={categoryList}
         selectedCategoryId={selectedCategoryId}
+        setSelectedCategoryId={setSelectedCategoryId}
       />
       <MenuList menuList={menuList} />
-      <MenuOptionSelector
+      {/* <MenuOptionSelector
         menu={mock[0].menuList[0]}
         optionList={mockOptionList}
       />
+      <Receipt order={order} /> */}
     </div>
   )
 }
--- packages/client/src/components/main/CategoryList.tsx
+++ packages/client/src/components/main/CategoryList.tsx
@@ -3,23 +3,29 @@ import { CategoryType } from '../../type'
 interface CategoryListProps {
   categoryList: CategoryType[]
   selectedCategoryId: number
+  setSelectedCategoryId: (id: number) => void
 }
 
 interface CategoryListItemProps {
   category: CategoryType
   isSelected: boolean
+  onClick: React.MouseEventHandler<HTMLLIElement>
 }
 
 function CategoryListItem({
   category: { id, name },
   isSelected,
+  onClick,
 }: CategoryListItemProps) {
-  return <li>{isSelected ? <strong>{name}</strong> : name}</li>
+  return (
+    <li onClick={onClick}>{isSelected ? <strong>{name}</strong> : name}</li>
+  )
 }
 
 export default function CategoryList({
   categoryList,
   selectedCategoryId,
+  setSelectedCategoryId,
 }: CategoryListProps) {
   return (
     <ul>
@@ -28,6 +34,9 @@ export default function CategoryList({
           key={category.id}
           category={category}
           isSelected={selectedCategoryId === category.id}
+          onClick={() => {
+            setSelectedCategoryId(category.id)
+          }}
         />
       ))}
     </ul>

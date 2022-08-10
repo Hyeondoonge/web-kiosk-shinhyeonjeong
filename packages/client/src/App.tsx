4e98f6d57b3ba560a67991fc095769ec2251491f
Hyeondoonge <migu554@naver.com>
2022-08-10 12:32:59 +0900

    feat: 모달 Context 생성 및 적용

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,16 +1,35 @@
 import CachePayment from 'components/main/CachePayment'
 import Cart from 'components/main/Cart'
 import CategoryList from 'components/main/CategoryList'
+import LoadingIndicator from 'components/main/LoadingIndicator'
 import MenuList from 'components/main/MenuList'
 import MenuOptionSelector from 'components/main/MenuOptionSelector'
 import ModalPortal from 'components/main/ModalPortal'
+import PaymentMethodSelector from 'components/main/PaymentMethodSelector'
 import Receipt from 'components/main/Receipt'
 import { getCartTotalAmount } from 'components/util'
-import React, { useEffect, useState } from 'react'
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  Dispatch,
+} from 'react'
 import { CartMenuType, CategoryType, MenuType, SelectedMenuType } from 'type'
 import mock from './mock/mockCategoryList.json'
 import mockOptionList from './mock/mockOptionList.json'
 import order from './mock/mockOrderedMenuList.json'
+
+type ModalOpenState = {
+  paymentMethod: boolean
+  cachePayment: boolean
+  loading: boolean
+  receipt: boolean
+}
+
+export const ModalContext = createContext<
+  [ModalOpenState, (modalOpenState: ModalOpenState) => void] | []
+>([])
 
 function App() {
   const [selectedCategoryId, setSelectedCategoryId] = useState(-1)
@@ -41,52 +60,116 @@ function App() {
     setCartMenuList([])
   }
 
+  const [isModalOpen, setIsModalOpen] = useState<ModalOpenState>({
+    paymentMethod: false,
+    cachePayment: false,
+    loading: false,
+    receipt: false,
+  })
+
+  const updateIsModalOpen = (newIsModalOpen: ModalOpenState) => {
+    setIsModalOpen(newIsModalOpen)
+  }
+
   const menuList =
     categoryList.filter(({ id }) => id === selectedCategoryId)[0]?.menuList ??
     []
 
-  const [isModalOpen, setIsModalOpen] = useState(false)
-
-  const closeModal = () => {
-    setIsModalOpen(false)
-  }
-
+  // 모달 상태 관리
   return (
-    <div className="App">
-      {/* <CategoryList
-        categoryList={categoryList}
-        selectedCategoryId={selectedCategoryId}
-        setSelectedCategoryId={setSelectedCategoryId}
-      />
-      <Cart
-        cartMenuList={cartMenuList}
-        updateCartMenuList={(newCartMenuList) => {
-          setCartMenuList(newCartMenuList)
-        }}
-        deleteAllCartMenu={deleteAllCartMenu}
-      /> */}
-      <MenuList
-        menuList={menuList}
-        onClick={() => {
-          setIsModalOpen(true)
-        }}
-      />
-      {isModalOpen && (
-        <ModalPortal closeModal={closeModal}>
-          <button onClick={closeModal}>닫기</button>
-          <MenuOptionSelector
-            menu={mock[0].menuList[0]}
-            optionList={mockOptionList}
-            updateSelectedMenuList={updateSelectedMenuList}
-            setIsModalOpen={setIsModalOpen}
-          />
-        </ModalPortal>
-      )}
-      {/* <Receipt order={order} deleteAllCartMenu={deleteAllCartMenu} /> */}
-      {/* <Modal>
-        <CachePayment orderAmount={getCartTotalAmount(cartMenuList)} />
-      </Modal> */}
-    </div>
+    <ModalContext.Provider value={[isModalOpen, updateIsModalOpen]}>
+      <div className="App">
+        {/* <CategoryList
+          categoryList={categoryList}
+          selectedCategoryId={selectedCategoryId}
+          setSelectedCategoryId={setSelectedCategoryId}
+        /> */}
+        <MenuList
+          menuList={menuList}
+          updateCartMenuList={(selectedMenu: SelectedMenuType) => {
+            const newSelectedMenuList = [...cartMenuList]
+
+            const cartMenu: CartMenuType = {
+              cartId: new Date().getTime(),
+              ...selectedMenu,
+            }
+            newSelectedMenuList.push(cartMenu)
+            setCartMenuList(newSelectedMenuList)
+          }}
+        />
+        <Cart
+          cartMenuList={cartMenuList}
+          updateCartMenuList={(newCartMenuList) => {
+            setCartMenuList(newCartMenuList)
+          }}
+          deleteAllCartMenu={deleteAllCartMenu}
+        />
+        {isModalOpen.paymentMethod && (
+          <ModalPortal
+            closeModal={() => {
+              setIsModalOpen({
+                ...isModalOpen,
+                paymentMethod: false,
+              })
+            }}
+          >
+            <button
+              onClick={() => {
+                setIsModalOpen({
+                  ...isModalOpen,
+                  paymentMethod: false,
+                })
+              }}
+            >
+              닫기
+            </button>
+            <PaymentMethodSelector />
+          </ModalPortal>
+        )}
+        {isModalOpen.cachePayment && (
+          <ModalPortal
+            closeModal={() => {
+              setIsModalOpen({ ...isModalOpen, cachePayment: false })
+            }}
+          >
+            <button
+              onClick={() => {
+                setIsModalOpen({ ...isModalOpen, cachePayment: false })
+              }}
+            >
+              닫기
+            </button>
+            <CachePayment orderAmount={getCartTotalAmount(cartMenuList)} />
+          </ModalPortal>
+        )}
+        {isModalOpen.loading && (
+          <ModalPortal
+            closeModal={() => {
+              setIsModalOpen({ ...isModalOpen, loading: false })
+            }}
+          >
+            <LoadingIndicator />
+          </ModalPortal>
+        )}
+        {isModalOpen.receipt && (
+          <ModalPortal
+            closeModal={() => {
+              setIsModalOpen({ ...isModalOpen, receipt: false })
+            }}
+          >
+            <button
+              onClick={() => {
+                deleteAllCartMenu()
+                setIsModalOpen({ ...isModalOpen, receipt: false })
+              }}
+            >
+              닫기
+            </button>
+            <Receipt order={order} deleteAllCartMenu={deleteAllCartMenu} />
+          </ModalPortal>
+        )}
+      </div>
+    </ModalContext.Provider>
   )
 }
 

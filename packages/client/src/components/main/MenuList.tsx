b84244765a5172bb19b633194b87d01f0815aeb7
Hyeondoonge <migu554@naver.com>
2022-08-10 12:38:24 +0900

    feat: 메뉴 클릭 시 메뉴 담기 모달 열기 기능

--- packages/client/src/components/main/MenuList.tsx
+++ packages/client/src/components/main/MenuList.tsx
@@ -1,33 +1,56 @@
-import { MouseEventHandler } from 'react'
-import { MenuType } from 'type'
+import { MouseEventHandler, useState } from 'react'
+import { MenuType, SelectedMenuType } from 'type'
+import MenuOptionSelector from './MenuOptionSelector'
+import ModalPortal from './ModalPortal'
+import mockOptionList from '../../mock/mockOptionList.json'
 
 interface MenuItemProps {
   isPopular: boolean
   menu: MenuType
-  onClick: MouseEventHandler<HTMLLIElement>
+  updateCartMenuList: (selectedMenu: SelectedMenuType) => void
 }
 
-function MenuListItem({
-  isPopular,
-  menu: { id, name, price, imgUrl },
-  onClick,
-}: MenuItemProps) {
+function MenuListItem({ isPopular, menu, updateCartMenuList }: MenuItemProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+  const { id, name, price, imgUrl } = menu
+
   return (
-    <li onClick={onClick}>
+    <li onClick={() => setIsModalOpen(true)}>
       <div>{isPopular ? '인기' : '인기없음'}</div>
       <img src={imgUrl} alt={name} />
       <div>{name}</div>
       <div>{price}</div>
+
+      {isModalOpen && (
+        <ModalPortal closeModal={closeModal}>
+          <button onClick={closeModal}>닫기</button>
+          <MenuOptionSelector
+            menu={menu}
+            optionList={mockOptionList} // menu를 전달해서 optionSelector에서 option가져오도록 수정
+            setIsModalOpen={setIsModalOpen}
+            updateCartMenuList={(selectedMenu: SelectedMenuType) => {
+              updateCartMenuList(selectedMenu)
+            }}
+          />
+        </ModalPortal>
+      )}
     </li>
   )
 }
 
 interface MenuListProps {
   menuList: MenuType[]
-  onClick: MouseEventHandler<HTMLLIElement>
+  updateCartMenuList: (selectedMenu: SelectedMenuType) => void
 }
 
-export default function MenuList({ menuList, onClick }: MenuListProps) {
+export default function MenuList({
+  menuList,
+  updateCartMenuList,
+}: MenuListProps) {
   const popularMenuId =
     [...menuList].sort((a, b) => b.totalSellCount - a.totalSellCount)[0]?.id ??
     -1
@@ -43,7 +66,7 @@ export default function MenuList({ menuList, onClick }: MenuListProps) {
           key={menu.id}
           menu={menu}
           isPopular={popularMenuId === menu.id}
-          onClick={onClick}
+          updateCartMenuList={updateCartMenuList}
         />
       ))}
     </ul>

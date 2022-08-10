e5d177a4fa27483cd6b791a199ffca32c66a820c
Hyeondoonge <migu554@naver.com>
2022-08-10 22:08:41 +0900

    feat: 재사용 Border 추가

---
+++ packages/client/src/components/common/Border.tsx
@@ -0,0 +1,10 @@
+import theme from 'style/theme'
+import styled from 'styled-components'
+
+const StyledBorder = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: ${theme.palette.line};
+`
+
+export default StyledBorder

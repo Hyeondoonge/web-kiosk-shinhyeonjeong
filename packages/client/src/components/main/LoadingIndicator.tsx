da139bc6f3403d9ae166f3b17e70486739b0f5c7
Hyeondoonge <migu554@naver.com>
2022-08-10 12:35:35 +0900

    feat: 로딩화면 - 모달 연결

--- packages/client/src/components/main/LoadingIndicator.tsx
+++ packages/client/src/components/main/LoadingIndicator.tsx
@@ -1,3 +1,30 @@
+import { ModalContext } from 'App'
+import { useContext, useEffect } from 'react'
+
 export default function LoadingIndicator() {
+  const [isModalOpen, setIsModalOpen] = useContext(ModalContext)
+  const onPaymentSuccess = () => {
+    if (!isModalOpen || !setIsModalOpen) return
+    setIsModalOpen({ ...isModalOpen, receipt: true, loading: false })
+  }
+
+  const onPaymentFail = () => {
+    if (!isModalOpen || !setIsModalOpen) return
+    setIsModalOpen({ ...isModalOpen, loading: false })
+    alert('결제 실패했습니다. 초기화면으로 돌아갑니다.')
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      const random = 0
+
+      if (random === 0) {
+        onPaymentFail()
+      } else {
+        onPaymentSuccess()
+      }
+    }, 500)
+  }, [])
+
   return <div>🫥 카드 결제중</div>
 }

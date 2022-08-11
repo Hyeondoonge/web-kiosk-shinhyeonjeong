d36120e6ab76637ca22833de2029e51b3575e379
HyeonDoonge <migu554@naver.com>
2022-08-11 16:37:47 +0900

    feat: menu entity 수정

--- packages/server/src/menus/entities/menu.entity.ts
+++ packages/server/src/menus/entities/menu.entity.ts
@@ -1,14 +1,21 @@
 import { Category } from 'src/categories/entities/category.entity'
+import { Option } from 'src/options/entities/option.entity'
 import {
   BaseEntity,
   Column,
   Entity,
   JoinColumn,
+  JoinTable,
+  ManyToMany,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
+  Unique,
 } from 'typeorm'
+import { SalesByDate } from './salesByDate.entity'
 
 @Entity({ name: 'menu_table' })
+@Unique(['name'])
 export class Menu extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number
@@ -28,4 +35,17 @@ export class Menu extends BaseEntity {
   @ManyToOne(() => Category, (category) => category.menu)
   @JoinColumn({ name: 'categoryId' })
   category: Category
+
+  @OneToMany(() => SalesByDate, (salesByDate) => salesByDate.menu, {
+    cascade: true,
+  })
+  salesByDate: SalesByDate[]
+
+  @ManyToMany(() => Option)
+  @JoinTable({
+    name: 'item_option_table',
+    joinColumn: { name: 'itemId', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'optionId', referencedColumnName: 'id' },
+  })
+  options: Option[]
 }

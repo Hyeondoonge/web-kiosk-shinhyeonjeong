7ba89d8e3ffa0ef2ee873af9004b2501fd270f58
HyeonDoonge <migu554@naver.com>
2022-08-11 16:07:54 +0900

    feat: orders entity, controller, service 추가

---
+++ packages/server/src/orders/entities/orderDetail.entity.ts
@@ -0,0 +1,37 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm'
+import { Order } from './order.entity'
+import { OrderDetailOption } from './orderDetailOption.entity'
+
+@Entity({ name: 'order_detail_table' })
+export class OrderDetail extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @Column()
+  orderNumber: number
+
+  @Column()
+  totalPrice: number
+
+  @ManyToOne(() => Order, (order) => order.orderDetailList)
+  @JoinColumn({ name: 'orderId' })
+  order: Order
+
+  @OneToMany(
+    () => OrderDetailOption,
+    (orderDetailOption) => orderDetailOption.orderDetail
+  )
+  orderDetailOptionList: OrderDetail[]
+}
